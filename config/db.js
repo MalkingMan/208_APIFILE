4cf32b50b5bd7e7b98ff6517aff2849f1073e294
MalkingMan <arrayalkhozini@gmail.com>
2025-11-25 10:03:06 +0700

    first commit

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const db = require('../models');
+
+asyync function connectDatabase() {
+    try {
+        await db.sequelize.authenticate();
+        console.log('Database Connected Successfully.');
+
+        await db.sequelize.sync({ alter: true });
+        console.log('Database synchronized');
+
+    } catch (err) {
+        console.error('Database Connection Failed', err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDatabase;
